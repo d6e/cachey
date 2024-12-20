78f37e87c1522790be4d86fd15379a74f004b687
Danielle Jenkins <danielle@megacrit.com>
2024-12-20 10:42:20 -0800

    Add usage to each command

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use clap::{Arg, Command};
 mod client;
 mod server;
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let matches = Command::new("cachey")
+fn build_cli() -> Command {
+    Command::new("cachey")
         .about("An arbitrary file caching system")
         .arg(
             Arg::new("url")
@@ -36,31 +35,57 @@ async fn main() -> std::io::Result<()> {
                 ),
         )
         .subcommand(Command::new("server").about("Server operations"))
-        .get_matches();
+}
 
-    let base_url = matches.get_one::<String>("url").unwrap().to_string();
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let mut main_cmd = build_cli();
+    let matches = main_cmd.clone().get_matches();
+
+    let base_url = matches.get_one::<String>("url").unwrap().clone();
 
     match matches.subcommand() {
-        Some(("client", client_matches)) => match client_matches.subcommand() {
-            Some(("upload", upload_matches)) => {
-                let files: Vec<String> = upload_matches
-                    .get_many::<String>("files")
-                    .unwrap()
-                    .map(|s| s.to_string())
-                    .collect();
-                client::upload(base_url, files).await
+        Some(("client", client_matches)) => {
+            match client_matches.subcommand() {
+                Some(("upload", upload_matches)) => {
+                    let files: Vec<_> = upload_matches
+                        .get_many::<String>("files")
+                        .unwrap()
+                        .cloned()
+                        .collect();
+                    client::upload(base_url, files).await
+                }
+                Some(("download", download_matches)) => {
+                    let files: Vec<_> = download_matches
+                        .get_many::<String>("files")
+                        .unwrap()
+                        .cloned()
+                        .collect();
+                    client::download(base_url, files).await
+                }
+                // If 'client' is used without specifying upload/download,
+                // print the help for the 'client' subcommand.
+                _ => {
+                    let mut client_cmd = build_cli();
+                    client_cmd
+                        .find_subcommand_mut("client")
+                        .unwrap()
+                        .print_help()?;
+                    println!();
+                    Ok(())
+                }
             }
-            Some(("download", download_matches)) => {
-                let files: Vec<String> = download_matches
-                    .get_many::<String>("files")
-                    .unwrap()
-                    .map(|s| s.to_string())
-                    .collect();
-                client::download(base_url, files).await
-            }
-            _ => Ok(()),
-        },
-        Some(("server", _)) => server::run(base_url).await,
-        _ => Ok(()),
+        }
+        Some(("server", _)) => {
+            // If server had sub-subcommands, handle them here
+            // For now, just run the server.
+            server::run(base_url).await
+        }
+        // If no subcommand is provided at all, print the main help.
+        _ => {
+            main_cmd.print_help()?;
+            println!();
+            Ok(())
+        }
     }
 }
